90ffae126072ccfa7a8b25f9ad554a3fe19071ca
r.almeida <rafael.almeida@rbmweb.com.br>
2023-03-16 21:09:40 -0300

    update: new version of base_tcc

---
+++ base2_tcc.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define blink_led 2
+
+const char* ssid = "Rafael - 2.4G";
+const char* password= "34627545";
+
+WebServer server(80); // create a web server on port 80
+
+void handleRoot() { // handle the root URL (/)
+  server.send(200, "text/html", "<h1>Hello World!</h1>");
+}
+
+void handleParams() { // handle URL with parameters (/params?param1=value1&param2=value2)
+  String param1Value = server.arg("param1"); // get the value of parameter 1
+  String param2Value = server.arg("param2"); // get the value of parameter 2
+
+  if (param1Value == "on") { // if parameter 1 is "on"
+    digitalWrite(blink_led, HIGH); // turn on the LED
+  } else if (param1Value == "off") { // if parameter 1 is "off"
+    digitalWrite(blink_led, LOW); // turn off the LED
+  }
+
+  if (param2Value.toInt() > 50) { // if parameter 2 is greater than 50
+    int i = 0;
+    while(i<20){
+      digitalWrite(blink_led, HIGH);
+      delay(100);
+      digitalWrite(blink_led, LOW);
+      delay(100);
+      i++;
+    }
+  }
+  else if (param2Value.toInt() <= 50) { // if parameter 2 is greater than 50
+      digitalWrite(blink_led, HIGH);
+      delay(2000);
+      digitalWrite(blink_led, LOW);
+  }
+  
+  server.send(200, "text/html", "Parameters received.<br><br>Param1Value: " + param1Value + "<br>Param2Value: " + param2Value + "<br>"); // send a response back to the client
+}
+
+void setup() {
+  pinMode(blink_led, OUTPUT); // set the built-in LED pin as an output
+  Serial.begin(115200); // start serial communication
+  WiFi.begin(ssid, password); // connect to Wi-Fi network
+
+  while (WiFi.status() != WL_CONNECTED) { // wait for connection
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); // print local IP address
+
+  server.on("/", handleRoot); // handle root URL
+  server.on("/params", handleParams); // handle URL with parameters
+
+  server.begin(); // start the server
+}
+
+void loop() {
+  server.handleClient(); // handle client requests
+}
