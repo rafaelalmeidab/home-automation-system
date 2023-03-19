0dbf7a312a89bc66c71a57efc94e4c3bb7c7d8bd
r.almeida <rafael.almeida@rbmweb.com.br>
2023-03-19 10:39:17 -0300

    update: new base tcc

--- base2_tcc/base2_tcc.ino
+++ base2_tcc/base2_tcc.ino
@@ -1,10 +1,16 @@
+//____params?param1=on&param2=value2
+
 #include <WiFi.h>
 #include <WebServer.h>
 
 #define blink_led 2
+#define relay 23
 
-const char* ssid = "Rafael - 2.4G";
-const char* password= "34627545";
+//const char* ssid = "Rafael - 2.4G";
+//const char* password= "34627545";
+
+const char* ssid = "Rio Cable - Rafael 2.4";
+const char* password= "rafael1061";
 
 WebServer server(80); // create a web server on port 80
 
@@ -15,13 +21,15 @@ void handleRoot() { // handle the root URL (/)
 void handleParams() { // handle URL with parameters (/params?param1=value1&param2=value2)
   String param1Value = server.arg("param1"); // get the value of parameter 1
   String param2Value = server.arg("param2"); // get the value of parameter 2
-
+  String relayParam  = server.arg("relay");
+  
   if (param1Value == "on") { // if parameter 1 is "on"
     digitalWrite(blink_led, HIGH); // turn on the LED
   } else if (param1Value == "off") { // if parameter 1 is "off"
     digitalWrite(blink_led, LOW); // turn off the LED
   }
 
+  /*
   if (param2Value.toInt() > 50) { // if parameter 2 is greater than 50
     int i = 0;
     while(i<20){
@@ -37,12 +45,21 @@ void handleParams() { // handle URL with parameters (/params?param1=value1&param
       delay(2000);
       digitalWrite(blink_led, LOW);
   }
+  */
   
-  server.send(200, "text/html", "Parameters received.<br><br>Param1Value: " + param1Value + "<br>Param2Value: " + param2Value + "<br>"); // send a response back to the client
+  //Relay Parameters
+  if (relayParam == "on") { // if parameter 1 is "on"
+    digitalWrite(relay, HIGH); // turn on the LED
+  } else if (relayParam == "off") { // if parameter 1 is "off"
+    digitalWrite(relay, LOW); // turn off the LED
+  }
+  
+  server.send(200, "text/html", "Parameters received.<br><br>Param1Value: " + param1Value + "<br>Param2Value: " + param2Value + "<br><br>Relay: " + relayParam + "<br>"); // send a response back to the client
 }
 
 void setup() {
   pinMode(blink_led, OUTPUT); // set the built-in LED pin as an output
+  pinMode(relay, OUTPUT);
   Serial.begin(115200); // start serial communication
   WiFi.begin(ssid, password); // connect to Wi-Fi network
 
