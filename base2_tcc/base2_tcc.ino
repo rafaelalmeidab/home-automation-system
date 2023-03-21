dfa516d8421ab3a3155356ba32f275aa9ade8141
r.almeida <rafael.almeida@rbmweb.com.br>
2023-03-21 13:08:48 -0300

    update: new parameters

--- base2_tcc/base2_tcc.ino
+++ base2_tcc/base2_tcc.ino
@@ -3,14 +3,18 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-#define blink_led 2
+#define esp32led 2
+#define infrared 22
 #define relay 23
 
 //const char* ssid = "Rafael - 2.4G";
 //const char* password= "34627545";
 
-const char* ssid = "Rio Cable - Rafael 2.4";
-const char* password= "rafael1061";
+//const char* ssid = "Rio Cable - Rafael 2.4";
+//const char* password= "rafael1061";
+
+const char* ssid = "RBMWEB";
+const char* password= "rbmweb01";
 
 WebServer server(80); // create a web server on port 80
 
@@ -19,47 +23,43 @@ void handleRoot() { // handle the root URL (/)
 }
 
 void handleParams() { // handle URL with parameters (/params?param1=value1&param2=value2)
-  String param1Value = server.arg("param1"); // get the value of parameter 1
-  String param2Value = server.arg("param2"); // get the value of parameter 2
-  String relayParam  = server.arg("relay");
-  
-  if (param1Value == "on") { // if parameter 1 is "on"
-    digitalWrite(blink_led, HIGH); // turn on the LED
-  } else if (param1Value == "off") { // if parameter 1 is "off"
-    digitalWrite(blink_led, LOW); // turn off the LED
-  }
+  String ESP32LedParam  = server.arg("esp32led");
+  String infraredParam  = server.arg("infrared");
+  String relayParam     = server.arg("relay");
 
-  /*
-  if (param2Value.toInt() > 50) { // if parameter 2 is greater than 50
-    int i = 0;
-    while(i<20){
-      digitalWrite(blink_led, HIGH);
-      delay(100);
-      digitalWrite(blink_led, LOW);
-      delay(100);
-      i++;
-    }
+  //ESP32Led Parameters
+  if (ESP32LedParam == "on") { 
+    digitalWrite(esp32ledlay, HIGH);
+  } 
+  else if (ESP32LedParam == "off") { 
+    digitalWrite(esp32led, LOW);
   }
-  else if (param2Value.toInt() <= 50) { // if parameter 2 is greater than 50
-      digitalWrite(blink_led, HIGH);
-      delay(2000);
-      digitalWrite(blink_led, LOW);
+  
+  //Infrared Parameters
+  if (infraredParam == "on") { 
+    digitalWrite(infrared, HIGH);
+  } 
+  else if (infraredParam == "off") { 
+    digitalWrite(infrared, LOW);
   }
-  */
   
   //Relay Parameters
-  if (relayParam == "on") { // if parameter 1 is "on"
-    digitalWrite(relay, HIGH); // turn on the LED
-  } else if (relayParam == "off") { // if parameter 1 is "off"
-    digitalWrite(relay, LOW); // turn off the LED
+  if (relayParam == "on") { 
+    digitalWrite(relay, HIGH);
+  } 
+  else if (relayParam == "off") { 
+    digitalWrite(relay, LOW);
   }
   
-  server.send(200, "text/html", "Parameters received.<br><br>Param1Value: " + param1Value + "<br>Param2Value: " + param2Value + "<br><br>Relay: " + relayParam + "<br>"); // send a response back to the client
+  //Send a response back to the client
+  server.send(200, "text/html", "Parameters received.<br><br>ESP32 Led: " + ESP32LedParam + "<br><br>Infrared: " + infraredParam + "<br><br>Relay: " + relayParam + "<br>"");
 }
 
-void setup() {
-  pinMode(blink_led, OUTPUT); // set the built-in LED pin as an output
+void setup() { 
+  pinMode(esp32led, OUTPUT); 
+  pinMode(infrared, OUTPUT); 
   pinMode(relay, OUTPUT);
+  
   Serial.begin(115200); // start serial communication
   WiFi.begin(ssid, password); // connect to Wi-Fi network
 
